7ada7a627706fc0a6283946078b645721135177a
sawzwe <sawjohnwaiyan@gmail.com>
2022-03-22 14:57:54 +0700

    new-server

--- app.js
+++ app.js
@@ -33,10 +33,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 //   res.sendFile(path.join(__dirname, "public", "index.html"));
 // });
 
-app.use(express.static(path.join(__dirname, "public")));
-app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname, "public","index.html"));
-});
+// app.use(express.static(path.join(__dirname, "public")));
+// app.get("/*", function (req, res) {
+//   res.sendFile(path.join(__dirname, "public","index.html"));
+// });
 
 // Plug routers
 app.use('/', indexRouter);
