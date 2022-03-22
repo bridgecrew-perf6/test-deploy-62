7ca4caaa9774cc17225a8fda49dbe87b387b2446
sawzwe <sawjohnwaiyan@gmail.com>
2022-03-22 13:28:37 +0700

    second commit

--- app.js
+++ app.js
@@ -28,6 +28,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use(express.static(path.join(__dirname, "public", "/")));
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
 // Plug routers
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
