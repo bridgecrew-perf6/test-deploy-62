71e3037e35db66efbdce2e6e0bd59fd98d358f93
sawzwe <sawjohnwaiyan@gmail.com>
2022-03-22 16:04:59 +0700

    This will work

--- app.js
+++ app.js
@@ -28,10 +28,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 //app.use(express.static(path.join(__dirname, 'build')));
 
-// app.use(express.static(path.join(__dirname, "public")));
-// app.get("//*", function (req, res) {
-//   res.sendFile(path.join(__dirname, "public", "index.html"));
-// });
+app.use(express.static(path.join(__dirname, "public")));
+app.get("//*", function (req, res) {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 // app.use(express.static(path.join(__dirname, "public","build")));
 // app.get("/", function (req, res) {
