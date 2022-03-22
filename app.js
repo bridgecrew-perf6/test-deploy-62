4109a4cbcb5d9840a1753f74131bdd625eabe19e
sawzwe <sawjohnwaiyan@gmail.com>
2022-03-22 16:17:24 +0700

    new commit

--- app.js
+++ app.js
@@ -30,7 +30,7 @@ app.use(cookieParser());
 
 app.use(express.static(path.join(__dirname, "public")));
 app.get("*", function (req, res) {
-  res.sendFile(pathgit.join(__dirname, "public", "index.html"));
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 // app.use(express.static(path.join(__dirname, "public","build")));
